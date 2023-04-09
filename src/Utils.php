81238da9c48e4a59435f820255cafdaa1db6d621
Fabian Wennink <contact@fabianwennink.nl>
2023-04-09 12:37:29 +0200

    Fixed UUID util not generating a valid UUID v4 string

--- src/Utils.php
+++ src/Utils.php
@@ -22,7 +22,10 @@ class Utils
      */
     public static function generateUUID(): string
     {
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4));
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     /**
